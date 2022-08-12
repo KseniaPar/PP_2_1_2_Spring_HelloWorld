a69d2e108931620d13a28cefea9934225e10a24c
KseniaPar <k.parpulova@gmail.com>
2022-08-12 19:02:31 +0500

    Task 2.1.2 is done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld beanHW1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHW2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("Comparison beans with Singleton scope: " + (beanHW1 == beanHW2));
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Comparison beans with Prototype scope: " + (beanCat1 == beanCat2));
     }
 }
